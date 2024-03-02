7037f5f3198c2522ce03be66fb369d6fcb621b1f
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-03-02 14:27:09 +0700

    Обнуление стилей в app.css, переменные цветов в _variables.scss. UI компонента Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import './header.scss'
+import React from "react";
+import "./header.scss";
 
 const Header = () => {
   return (
-    <div className='header'>
-      header
+    <div className="header">
+      <h1 className="header__title">Калькулятор раздельного бюджета</h1>
+      <h2 className="header__subtitle">
+        Не порть отношения вопросом, кто будет считать чеки. <br/>Пусть ими займется
+        программа.
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
